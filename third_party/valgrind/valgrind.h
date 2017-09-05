07b14b0e3834b4bedf5d156dba18ae2938ba6b33
spezi77 <spezi77@gmx.com>
2017-09-05 23:43:29 +0200

    Patched for compiling with twrp.
    Change-Id: I756a25683bcf2ab22cdd7e18e0b09a17d83b9b73
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- third_party/valgrind/valgrind.h
+++ third_party/valgrind/valgrind.h
@@ -1,6 +1,2 @@
-#ifdef ANDROID
-  #include "include/valgrind.h"
-#else
-  // These files will be added in a patch applied in the ebuild.
-  #include <base/third_party/valgrind/valgrind.h>
-#endif
+#include <base/third_party/valgrind/valgrind.h>
+
